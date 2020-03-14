1e340d645626ecd38f205d9507a34bdd55652c4f
marsathoriq <13517071@std.stei.itb.ac.id>
2020-03-14 19:11:05 +0700

    change shading

--- src/draw.cpp
+++ src/draw.cpp
@@ -29,15 +29,18 @@ float red = 1.0f, blue = 1.0f, green = 1.0f;
 
 static bool shade = false;
 
-const GLfloat light_ambient[] = {0.0f, 5.0f, 7.0f, 10.0f};
-const GLfloat light_diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
-const GLfloat light_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
-const GLfloat light_position[] = {2.0f, 5.0f, 5.0f, 0.0f};
+GLfloat ambientLight[] = {0.2f, 0.2f, 0.2f, 1.0f};
+GLfloat lightColor[] = {0.6f, 0.6f, 0.6f, 1.0f};
+GLfloat lightPos[] ={10,0,-15,1};
 
-const GLfloat mat_ambient[] = {0.7f, 0.7f, 0.7f, 1.0f};
-const GLfloat mat_diffuse[] = {0.8f, 0.8f, 0.8f, 1.0f};
-const GLfloat mat_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
-const GLfloat high_shininess[] = {100.0f};
+  //The color of the sphere
+GLfloat materialColor[] = {1.0f, 1.0f, 0.0f, 1.0f};
+//The specular (shiny) component of the material
+GLfloat materialSpecular[] = {0,0,1,1};
+//The color emitted by the material
+GLfloat materialEmission[] = {1.0f,1.0f,0, 1.0f};
+
+GLfloat shininess = 20;
 
 void reset_display()
 {
@@ -175,33 +178,32 @@ void DrawCube(void)
     glRotated(xRotated, 1.0, 0.0, 0.0);
     glRotated(yRotated, 0.0, 1.0, 0.0);
     glRotated(zRotated, 0.0, 0.0, 1.0);
-        if (shade)
-    {
-        // printf("1");
-        glEnable(GL_LIGHT0);
-        glEnable(GL_NORMALIZE);
-        glEnable(GL_COLOR_MATERIAL);
-        glEnable(GL_LIGHTING);
+    
+	if (shade){
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+		glEnable(GL_NORMALIZE);
+		//Disable color materials, so that glMaterial calls work
+		glDisable(GL_COLOR_MATERIAL);
+		//Diffuse (non-shiny) light component
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
+		//Specular (shiny) light component
+		glLightfv(GL_LIGHT0, GL_SPECULAR, lightColor);
+		glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
 
-        glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-        glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-        glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-        glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-        glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-        glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-        glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-    }
-    else
-    {
-        // printf("0");
-
-        glDisable(GL_LIGHT0);
-        glDisable(GL_NORMALIZE);
-        glDisable(GL_COLOR_MATERIAL);
-        glDisable(GL_LIGHTING);
-    }
+		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, materialColor);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, materialSpecular);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, materialEmission);
+		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess); //The shininess parameter
+	}else{
+		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_LIGHTING);
+		glDisable(GL_LIGHT0);
+		GLdouble(GL_NORMALIZE);
+		//Disable color materials, so that glMaterial calls work
+		glEnable(GL_COLOR_MATERIAL);
+	}
     // glTranslatef(0.0, 0.0, -10.5);
     // glutSolidCube(6);
     glBegin(GL_QUADS);
