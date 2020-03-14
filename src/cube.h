a13ab52f1cc82a839d0e168bf046de1581b402f0
ramadhanriandi <izurbudianto@gmail.com>
2020-03-14 10:25:24 +0700

    init parser code for obj file

---
+++ src/cube.h
@@ -0,0 +1,13 @@
+// This is a .h file for the model: cube
+
+// Positions: 8
+// Texels: 14
+// Normals: 6
+// Faces: 6
+// Vertices: 18
+
+const int cubeVertices;
+const float cubePositions[54];
+const float cubeTexels[36];
+const float cubeNormals[54];
+
