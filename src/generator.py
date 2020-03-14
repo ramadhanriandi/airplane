d9a803fbe65db9d09593ce09a33fd9b907fcbd91
bmusuko <bram.musuko@gmail.com>
2020-03-14 21:56:08 +0700

    add generator

--- src/generator.py
+++ src/generator.py
@@ -1,32 +1,55 @@
-def generate(a,b,c,d,e,f,g,h):
-    print("glVertex3d("+str(a[0])+"f,"+str(a[1])+"f,"+str(a[2])+"f);")
-    print("glVertex3d("+str(b[0])+"f,"+str(b[1])+"f,"+str(b[2])+"f);")
-    print("glVertex3d("+str(c[0])+"f,"+str(c[1])+"f,"+str(c[2])+"f);")
-    print("glVertex3d("+str(d[0])+"f,"+str(d[1])+"f,"+str(d[2])+"f);")
+def generate(vertex_temp,file_output):
+    file_output.write(str(vertex_temp[0][0])+","+str(vertex_temp[0][1])+","+str(vertex_temp[0][2])+",")
+    file_output.write(str(vertex_temp[1][0])+","+str(vertex_temp[1][1])+","+str(vertex_temp[1][2])+",")
+    file_output.write(str(vertex_temp[2][0])+","+str(vertex_temp[2][1])+","+str(vertex_temp[2][2])+",")
+    file_output.write(str(vertex_temp[3][0])+","+str(vertex_temp[3][1])+","+str(vertex_temp[3][2])+",")
 
-    print("glVertex3d("+str(a[0])+"f,"+str(a[1])+"f,"+str(a[2])+"f);")
-    print("glVertex3d("+str(b[0])+"f,"+str(b[1])+"f,"+str(b[2])+"f);")
-    print("glVertex3d("+str(f[0])+"f,"+str(f[1])+"f,"+str(f[2])+"f);")
-    print("glVertex3d("+str(e[0])+"f,"+str(e[1])+"f,"+str(e[2])+"f);")
+    file_output.write(str(vertex_temp[0][0])+","+str(vertex_temp[0][1])+","+str(vertex_temp[0][2])+",")
+    file_output.write(str(vertex_temp[1][0])+","+str(vertex_temp[1][1])+","+str(vertex_temp[1][2])+",")
+    file_output.write(str(vertex_temp[5][0])+","+str(vertex_temp[5][1])+","+str(vertex_temp[5][2])+",")
+    file_output.write(str(vertex_temp[4][0])+","+str(vertex_temp[4][1])+","+str(vertex_temp[4][2])+",")
 
-    print("glVertex3d("+str(c[0])+"f,"+str(c[1])+"f,"+str(c[2])+"f);")
-    print("glVertex3d("+str(d[0])+"f,"+str(d[1])+"f,"+str(d[2])+"f);")
-    print("glVertex3d("+str(h[0])+"f,"+str(h[1])+"f,"+str(h[2])+"f);")
-    print("glVertex3d("+str(g[0])+"f,"+str(g[1])+"f,"+str(g[2])+"f);")
+    file_output.write(str(vertex_temp[2][0])+","+str(vertex_temp[2][1])+","+str(vertex_temp[2][2])+",")
+    file_output.write(str(vertex_temp[3][0])+","+str(vertex_temp[3][1])+","+str(vertex_temp[3][2])+",")
+    file_output.write(str(vertex_temp[7][0])+","+str(vertex_temp[7][1])+","+str(vertex_temp[7][2])+",")
+    file_output.write(str(vertex_temp[6][0])+","+str(vertex_temp[6][1])+","+str(vertex_temp[6][2])+",")
 
-    print("glVertex3d("+str(a[0])+"f,"+str(a[1])+"f,"+str(a[2])+"f);")
-    print("glVertex3d("+str(d[0])+"f,"+str(d[1])+"f,"+str(d[2])+"f);")
-    print("glVertex3d("+str(h[0])+"f,"+str(h[1])+"f,"+str(h[2])+"f);")
-    print("glVertex3d("+str(e[0])+"f,"+str(e[1])+"f,"+str(e[2])+"f);")
+    file_output.write(str(vertex_temp[0][0])+","+str(vertex_temp[0][1])+","+str(vertex_temp[0][2])+",")
+    file_output.write(str(vertex_temp[3][0])+","+str(vertex_temp[3][1])+","+str(vertex_temp[3][2])+",")
+    file_output.write(str(vertex_temp[7][0])+","+str(vertex_temp[7][1])+","+str(vertex_temp[7][2])+",")
+    file_output.write(str(vertex_temp[4][0])+","+str(vertex_temp[4][1])+","+str(vertex_temp[4][2])+",")
 
-    print("glVertex3d("+str(b[0])+"f,"+str(b[1])+"f,"+str(b[2])+"f);")
-    print("glVertex3d("+str(c[0])+"f,"+str(c[1])+"f,"+str(c[2])+"f);")
-    print("glVertex3d("+str(g[0])+"f,"+str(g[1])+"f,"+str(g[2])+"f);")
-    print("glVertex3d("+str(f[0])+"f,"+str(f[1])+"f,"+str(f[2])+"f);")
+    file_output.write(str(vertex_temp[1][0])+","+str(vertex_temp[1][1])+","+str(vertex_temp[1][2])+",")
+    file_output.write(str(vertex_temp[2][0])+","+str(vertex_temp[2][1])+","+str(vertex_temp[2][2])+",")
+    file_output.write(str(vertex_temp[6][0])+","+str(vertex_temp[6][1])+","+str(vertex_temp[6][2])+",")
+    file_output.write(str(vertex_temp[5][0])+","+str(vertex_temp[5][1])+","+str(vertex_temp[5][2])+",")
 
-    print("glVertex3d("+str(e[0])+"f,"+str(e[1])+"f,"+str(e[2])+"f);")
-    print("glVertex3d("+str(f[0])+"f,"+str(f[1])+"f,"+str(f[2])+"f);")
-    print("glVertex3d("+str(g[0])+"f,"+str(g[1])+"f,"+str(g[2])+"f);")
-    print("glVertex3d("+str(h[0])+"f,"+str(h[1])+"f,"+str(h[2])+"f);")
+    file_output.write(str(vertex_temp[4][0])+","+str(vertex_temp[4][1])+","+str(vertex_temp[4][2])+",")
+    file_output.write(str(vertex_temp[5][0])+","+str(vertex_temp[5][1])+","+str(vertex_temp[5][2])+",")
+    file_output.write(str(vertex_temp[6][0])+","+str(vertex_temp[6][1])+","+str(vertex_temp[6][2])+",")
+    file_output.write(str(vertex_temp[7][0])+","+str(vertex_temp[7][1])+","+str(vertex_temp[7][2])+",")
 
 
+counter = 0
+vertex_temp = []
+file_output = open("plane.c", "w")
+file_output.write('#include "plane.h"')
+file_output.write('extern const float planePositions[576]) = ')
+file_output.write('{')
+with open("input.txt") as f:
+    for line in f:
+        line = line.rstrip()
+        if line == "//":
+            continue
+        if counter == 0:
+            vertex_temp = []
+            counter += 1
+            vertex_temp.append([float(x) for x in line.split(',')])
+        if (counter == 8):
+            generate(vertex_temp, file_output)
+            counter = 0
+file_output.write('};')
+file_output.close()
+file_output2 = open("plane.h","w")
+file_output2.write("extern const float planev2Positions[540];")
+file_output2.close()
