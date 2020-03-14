da1ad3575b9547f2f93ff79efad9827b3595c1f6
ramadhanriandi <izurbudianto@gmail.com>
2020-03-14 11:47:02 +0700

    integrate plane obj to draw

--- src/draw.cpp
+++ src/draw.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <cstdint>
 #include <cstring>
-#include "cube.c"
+#include "plane.c"
 
 using namespace std;
 
@@ -25,41 +25,41 @@ void DrawCube(void) {
 
     glBegin(GL_QUADS);
 
-    int numberOfVertex = sizeof(cubePositions)/sizeof(*cubePositions);
+    int numberOfVertex = sizeof(planePositions)/sizeof(*planePositions);
 
     for (int i = numberOfVertex - 1; i >= 0; i-=3) {
-        float firstVertex = cubePositions[i-2];
-        float secondVertex = cubePositions[i - 1];
-        float thirdVertex = cubePositions[i];
+        float firstVertex = planePositions[i-2];
+        float secondVertex = planePositions[i - 1];
+        float thirdVertex = planePositions[i];
         glVertex3f(firstVertex, secondVertex, thirdVertex);
     }
 
-    // // glColor3f(0.0f,1.0f,0.0f);    // Color Blue
+    glColor3f(0.0f,1.0f,0.0f);    // Color Blue
     // glVertex3f( 1.0f, 1.0f,-1.0f);    // Top Right Of The Quad (Top)
     // glVertex3f(-1.0f, 1.0f,-1.0f);    // Top Left Of The Quad (Top)
     // glVertex3f(-1.0f, 1.0f, 1.0f);    // Bottom Left Of The Quad (Top)
     // glVertex3f( 1.0f, 1.0f, 1.0f);    // Bottom Right Of The Quad (Top)
-    // // glColor3f(1.0f,0.5f,0.0f);    // Color Orange
+    glColor3f(1.0f,0.5f,0.0f);    // Color Orange
     // glVertex3f( 1.0f,-1.0f, 1.0f);    // Top Right Of The Quad (Bottom)
     // glVertex3f(-1.0f,-1.0f, 1.0f);    // Top Left Of The Quad (Bottom)
     // glVertex3f(-1.0f,-1.0f,-1.0f);    // Bottom Left Of The Quad (Bottom)
     // glVertex3f( 1.0f,-1.0f,-1.0f);    // Bottom Right Of The Quad (Bottom)
-    // // glColor3f(1.0f,0.0f,0.0f);    // Color Red    
+    glColor3f(1.0f,0.0f,0.0f);    // Color Red    
     // glVertex3f( 1.0f, 1.0f, 1.0f);    // Top Right Of The Quad (Front)
     // glVertex3f(-1.0f, 1.0f, 1.0f);    // Top Left Of The Quad (Front)
     // glVertex3f(-1.0f,-1.0f, 1.0f);    // Bottom Left Of The Quad (Front)
     // glVertex3f( 1.0f,-1.0f, 1.0f);    // Bottom Right Of The Quad (Front)
-    // // glColor3f(1.0f,1.0f,0.0f);    // Color Yellow
+    glColor3f(1.0f,1.0f,0.0f);    // Color Yellow
     // glVertex3f( 1.0f,-1.0f,-1.0f);    // Top Right Of The Quad (Back)
     // glVertex3f(-1.0f,-1.0f,-1.0f);    // Top Left Of The Quad (Back)
     // glVertex3f(-1.0f, 1.0f,-1.0f);    // Bottom Left Of The Quad (Back)
     // glVertex3f( 1.0f, 1.0f,-1.0f);    // Bottom Right Of The Quad (Back)
-    // // glColor3f(0.0f,0.0f,1.0f);    // Color Blue
+    glColor3f(0.0f,0.0f,1.0f);    // Color Blue
     // glVertex3f(-1.0f, 1.0f, 1.0f);    // Top Right Of The Quad (Left)
     // glVertex3f(-1.0f, 1.0f,-1.0f);    // Top Left Of The Quad (Left)
     // glVertex3f(-1.0f,-1.0f,-1.0f);    // Bottom Left Of The Quad (Left)
     // glVertex3f(-1.0f,-1.0f, 1.0f);    // Bottom Right Of The Quad (Left)
-    // // glColor3f(1.0f,0.0f,1.0f);    // Color Violet
+    glColor3f(1.0f,0.0f,1.0f);    // Color Violet
     // glVertex3f( 1.0f, 1.0f,-1.0f);    // Top Right Of The Quad (Right)
     // glVertex3f( 1.0f, 1.0f, 1.0f);    // Top Left Of The Quad (Right)
     // glVertex3f( 1.0f,-1.0f, 1.0f);    // Bottom Left Of The Quad (Right)
@@ -85,7 +85,6 @@ void reshape(int x, int y) {
 }
 
 int main(int argc, char** argv) {
-    cout << cubePositions[0] << endl;
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
     glutInitWindowPosition(100, 100);
---
+++ src/plane.h
@@ -0,0 +1,13 @@
+// This is a .h file for the model: plane
+
+// Positions: 501
+// Texels: 594
+// Normals: 351
+// Faces: 487
+// Vertices: 1461
+
+extern const int planeVertices;
+extern const float planePositions[4383];
+extern const float planeTexels[2922];
+extern const float planeNormals[4383];
+
